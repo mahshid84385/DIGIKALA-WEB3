edf179f42e60674ad78a4616a5c0a15910ed97bc
mahshid84385 <mahroo.2060@gmail.com>
2026-08-28 23:35:43 +0330

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/DIGIKALA-WEB3/",
 
-  base:"/digikala-web3/",
-
-  plugins:[
-    tailwindcss()
-  ],
-
-})
+  plugins: [tailwindcss()],
+});
